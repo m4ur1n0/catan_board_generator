7c49f920c5908ddfa4a55e1a36d8138167cc142a
theo <macuser@Theos-MacBook-2.local>
2024-08-09 17:18:08 -0400

    removed python scripts

--- kill_servers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Kill all processes running 'npm run dev'
-pkill -f 'npm run dev' &
-pkill -f ' server.py' &
-
-echo "Killed all processes running 'npm run dev' or 'server.py'"
--- test.py
+++
@@ -1,5 +0,0 @@
-import get_tile_numbers
-import get_resource_locations
-
-tiles_list = get_tile_numbers.get_nums()
-get_resource_locations.get_locs(tiles_list)
