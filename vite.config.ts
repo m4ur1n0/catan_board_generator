5bb3a9aa15a643e3e0a37fb913fa31dcd4d48018
theo <macuser@Theos-MacBook-2.local>
2024-09-20 16:28:35 -0500

    added base:

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/catan_board_generator/',
   plugins: [react()],
   build: {
     outDir: 'dist', // Ensure this matches the output directory you want
